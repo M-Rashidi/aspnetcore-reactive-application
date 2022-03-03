ea174be03159019e79e32484b3f4a5d404660b68
Mohammad Hasan Rashidi <mrrashidi@outlook.com>
2022-03-04 02:27:51 +0330

    added producer & consumer for publish subscribe communication

---
+++ message-driven/message-channel/publish-subscribe/rabbitmq-sample/consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace consumer
+{
+    class Program
+    {
+        private static IConnection? _connection;
+        private static IModel? _channel;
+        private static readonly ManualResetEvent _resetEvent = new ManualResetEvent(false);
+        private static readonly string url = "amqp://guest:guest@localhost/%2f";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(url)
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            var exchangeName = "publish-subscribe";
+            var routingKey = "";
+
+            var queueName = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: routingKey);
+
+            var consumer = new EventingBasicConsumer(_channel);
+
+
+            consumer.Received += (model, deliveryEventArgs) =>
+            {
+                var body = deliveryEventArgs.Body.ToArray();
+
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("** Received message: {0} by Consumer thread **", message);
+
+                _channel.BasicAck(deliveryEventArgs.DeliveryTag, false);
+            };
+
+            _ = _channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            _resetEvent.WaitOne();
+            _channel?.Close();
+            _channel = null;
+            _connection?.Close();
+            _connection = null;
+        }
+    }
+}
---
+++ message-driven/message-channel/publish-subscribe/rabbitmq-sample/producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace producer
+{
+    class Program
+    {
+        private static readonly string url = "amqp://guest:guest@localhost/%2f";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(url)
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var exchangeName = "publish-subscribe";
+            var exchangeType = ExchangeType.Fanout;
+
+            channel.ExchangeDeclare(exchange: exchangeName, type: exchangeType);
+
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            var routingKey = "";
+
+            channel.BasicPublish(exchangeName, routingKey, null, data);
+
+            ;
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+                ? string.Join(" ", args)
+                : "info: Hello World!");
+        }
+    }
+}
