446fdc2e817168dec6d764b7131bc9064946910f
Mohammad Hasan Rashidi <mrrashidi@outlook.com>
2022-03-03 13:47:42 +0330

    added producer for point to point communication

---
+++ message-driven/message-channel/point-to-point/rabbitmq-sample/producer/producer/producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace producer
+{
+    class Program
+    {
+        private static readonly string url = "amqp://guest:guest@localhost/%2f";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(url)
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var queueName = "point-to-point-queue";
+            bool durable = false;
+            bool exclusive = false;
+            bool autoDelete = true;
+
+            channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            var exchangeName = "";
+            var routingKey = queueName;
+            channel.BasicPublish(exchangeName, routingKey, null, data);
+        }
+    }
+}
