77b0946b294ddcbd7db772419b94240db978245a
Mohammad Hasan Rashidi <mrrashidi@outlook.com>
2022-03-05 16:38:23 +0330

    added producer & consumer for datatype communication

---
+++ message-driven/message-channel/datatype-channel/rabbitmq-sample/consumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace consumer
+{
+    class Program
+    {
+        private static IConnection? _connection;
+        private static IModel? _channel;
+        private static readonly ManualResetEvent _resetEvent = new ManualResetEvent(false);
+        private static readonly string url = "amqps://zmkoitrj:X5Pa_v8emJMUHzzbiL17QNoh0j52HvUo@baboon.rmq.cloudamqp.com/zmkoitrj";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(url)
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            var exchangeName = "Product";
+            var productQueries = "ProductQueries";
+            var productPurchase = "ProductPurchase";
+            var productQuote = "ProductQuote";
+
+
+            var queueName = _channel.QueueDeclare().QueueName;
+
+            _channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: productQueries);
+            _channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: productPurchase);
+            _channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: productQuote);
+
+            var consumer = new EventingBasicConsumer(_channel);
+
+
+            consumer.Received += (model, deliveryEventArgs) =>
+            {
+                var body = deliveryEventArgs.Body.ToArray();
+
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("** Received message: {0} by Consumer thread in **", message);
+
+                _channel.BasicAck(deliveryEventArgs.DeliveryTag, false);
+            };
+
+            _ = _channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            _resetEvent.WaitOne();
+            _channel?.Close();
+            _channel = null;
+            _connection?.Close();
+            _connection = null;
+        }
+    }
+}
---
+++ message-driven/message-channel/datatype-channel/rabbitmq-sample/producer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace producer
+{
+    class Program
+    {
+        private static readonly string url = "amqps://zmkoitrj:X5Pa_v8emJMUHzzbiL17QNoh0j52HvUo@baboon.rmq.cloudamqp.com/zmkoitrj";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(url)
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var exchangeName = "Product";
+
+            var exchangeType = ExchangeType.Direct;
+
+            channel.ExchangeDeclare(exchange: exchangeName, type: exchangeType);
+
+            var messages = new List<DomainEvent>
+            {
+                new ProductQueries(),
+                new ProductPurchase(),
+                new ProductQuote()
+            };
+
+            foreach (var _message in messages)
+            {
+                var message = JsonConvert.SerializeObject(_message);
+                var data = Encoding.UTF8.GetBytes(message);
+                var routingKey = _message.GetType().Name;
+
+                channel.BasicPublish(exchangeName, routingKey, null, data);
+            }
+        }
+    }
+
+    public class DomainEvent
+    {
+    }
+
+    public class ProductQueries : DomainEvent
+    {
+        public ProductQueries()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+    public class ProductQuote : DomainEvent
+    {
+        public ProductQuote()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+    public class ProductPurchase : DomainEvent
+    {
+        public ProductPurchase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
