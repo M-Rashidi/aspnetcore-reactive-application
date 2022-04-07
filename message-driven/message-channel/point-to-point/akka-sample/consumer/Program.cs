7436d6be6242a5d9e52c52a23b4a50f3862734f2
Mohammad Hasan Rashidi <mrrashidi@outlook.com>
2022-04-07 15:37:02 +0430

    adding an example of a point to point communication with akka

---
+++ message-driven/message-channel/point-to-point/akka-sample/consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+                akka {  
+                    actor {
+                        provider = remote
+                    }
+                    remote {
+                        dot-netty.tcp {
+                            port = 8081
+                            hostname = 0.0.0.0
+                            public-hostname = localhost
+                        }
+                    }
+                }
+                ");
+
+            using var system = ActorSystem.Create("consumer-system", config);
+
+            var consumerActor = system.ActorOf<ConsumerActor>("ConsumerActor");
+
+            Console.ReadLine();
+        }
+
+    }
+
+
+    public class ConsumerActor : ReceiveActor
+    {
+        public ConsumerActor()
+        {
+            var x = Self.Path.ToString();
+            Receive<string>(message => Console.WriteLine($" Received message: {message}", ConsoleColor.Green));
+        }
+    }
+}
---
+++ message-driven/message-channel/point-to-point/akka-sample/producer/Program.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+
+namespace producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+                akka {  
+                    actor {
+                        provider = remote
+                    }
+                    remote {
+                        dot-netty.tcp {
+                            port = 8082
+                            hostname = 0.0.0.0
+                            public-hostname = localhost
+                        }
+                    }
+                }
+                ");
+
+            using var system = ActorSystem.Create("producer-system", config);
+                
+            var producerActor = system.ActorOf<ProducerActor>("ProducerActor");
+
+            while (true)
+            {
+                producerActor.Tell(Console.ReadLine());
+            }
+        }
+    }
+
+
+    public class ProducerActor : ReceiveActor
+    {
+        public ProducerActor()
+        {
+            Receive<string>(message =>
+            {
+                var selectionConsumer = Context.ActorSelection("akka.tcp://consumer-system@localhost:8081/user/ConsumerActor");
+
+                selectionConsumer.Tell(message);
+            });
+        }
+    }
+}
